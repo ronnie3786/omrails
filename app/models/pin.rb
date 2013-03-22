78926a73a0b3c234817937d41a08fb2b7751b47a
Ronnie Rocha <ronnie3786@gmail.com>
2013-03-22 11:18:09 -0500

    Added attr_accessor to Pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,6 +10,9 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "320x240>" }
 
-  attr_accessible :image_file_name
+  attr_accessor :image_file_name 
+	attr_accessor :image_content_type 
+	attr_accessor :image_file_size 
+	attr_accessor :image_updated_at 
 
 end
